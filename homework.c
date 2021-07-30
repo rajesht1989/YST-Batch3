913ac51eb89dca3768707a67339288a4f8cc4c6a
Nagarasu0408 <88180695+Nagarasu0408@users.noreply.github.com>
2021-07-30 20:45:10 +0530

    Update homework.c

--- homework.c
+++ homework.c
@@ -1,3 +1,5 @@
+//INTEGER
+
 //REG:GET 2 NUMBERS AND PRINT TWO NUMBER'S  ADD,SUBTRACTION,MALTIPILE,AND DEVIDE VALUE
 
 #include <stdio.h>
@@ -18,6 +20,36 @@ printf("\n\tTwo Number Maltipile   Result %d",s);
 int d=q/w;
 printf("\n\tTwo Number Divide  Result %d",d);
 }
+
+
+
+//REG:GET 3 NUMBER AND PRINT  ADD,SUBTRACTION,MALTIPILE,AND DEVIDE 3 NUMBER'S  VALUE
+
+#include <stdio.h>
+
+int main(){
+int q;
+int w;
+int e;
+printf("First Number ");
+scanf("%d",&q);
+printf("\nSecond Number ");
+scanf("%d",&w);
+printf("\nThird Number ");
+scanf("%d",&e);
+int r=q+w+e;
+printf("\n\tThree Number Add Result %d",r);
+int a=q-w-e;
+printf("\n\tThree Number Minus  Result %d",a);
+int s=q*w*e;
+printf("\n\tThree Number Maltipile   Result %d",s);
+int d=q/(w/e);
+printf("\n\tThree Number Divide  Result %d",d);
+}
+
+
+
+
 
 //FLOAT
 
@@ -42,3 +74,26 @@ float d=q/w;
 printf("\n\tTwo Decimal Number Divide  Result %f",d);
 }
 
+//REG:GET 3 DECIMAL NUMBERS AND PRINT  ADD,SUBTRACTION,MALTIPILE,AND DEVIDE DECIMAL 3 NUMBER'S  VALUE
+
+#include <stdio.h>
+
+int main(){
+float q;
+float w;
+float e;
+printf("First Decimal Number ");
+scanf("%f",&q);
+printf("\nSecond Decimal Number ");
+scanf("%f",&w);
+printf("\nThird Decimal Number ");
+scanf("%f",&e);
+float r=q+w+e;
+printf("\n\tThree Decimal Number Add Result %f",r);
+float a=q-w-e;
+printf("\n\tThree Decimal Number Minus  Result %f",a);
+float s=q*w*e;
+printf("\n\tThree Decimal Number Maltipile   Result %f",s);
+float d=q/(w/e);
+printf("\n\tThree Decimal Number Divide  Result %f",d);
+}
