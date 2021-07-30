9aedb4b0ee2d2b0b7dcdfc394b061abc547c6ae8
Rajesh <rajesht1989@gmail.com>
2021-07-30 12:07:00 +0530

    Create Add2nos.c

---
+++ Add2nos.c
@@ -0,0 +1,12 @@
+// get 2 number from user add them and print the result
+#include <stdio.h>
+void main (){
+int firstno;
+int secondno;
+printf("Enter first number ");
+scanf("%d",&firstno);
+printf("Enter second number ");
+scanf("%d",&secondno);
+int result=firstno+secondno;
+printf("Addtion of two numbers are %d", result);
+}
