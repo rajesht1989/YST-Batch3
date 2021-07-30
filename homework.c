51fc6b939d431f43e6485c8886748e599146f831
Nagarasu0408 <88180695+Nagarasu0408@users.noreply.github.com>
2021-07-30 20:31:52 +0530

    Update homework.c

--- homework.c
+++ homework.c
@@ -18,3 +18,27 @@ printf("\n\tTwo Number Maltipile   Result %d",s);
 int d=q/w;
 printf("\n\tTwo Number Divide  Result %d",d);
 }
+
+//FLOAT
+
+//REG:GET 2 DECIMAL NUMBERS AND PRINT  ADD,SUBTRACTION,MALTIPILE,AND DEVIDE DECIMAL TWO NUMBER'S  VALUE
+
+#include <stdio.h>
+
+int main(){
+float q;
+float w;
+printf("First Decimal Number ");
+scanf("%f",&q);
+printf("\nSecond Decimal Number ");
+scanf("%f",&w);
+float r=q+w;
+printf("\n\tTwo Decimal Number Add Result %f",r);
+float a=q-w;
+printf("\n\tTwo Decimal Number Minus  Result %f",a);
+float s=q*w;
+printf("\n\tTwo Decimal Number Maltipil   Result %f",s);
+float d=q/w;
+printf("\n\tTwo Decimal Number Divide  Result %f",d);
+}
+
