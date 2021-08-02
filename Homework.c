dac658ef7b469d93d214e517f5ec02a64a2463da
arunmrya2129 <88180426+arunmrya2129@users.noreply.github.com>
2021-08-02 10:04:43 +0530

    Create Homework.c

---
+++ Homework.c
@@ -0,0 +1,84 @@
+// get two values from the user and print it and scan it
+#include <stdio.h>
+int main() {
+int firstno;
+int secondno;
+printf("Enter the firstno ");
+scanf("%d",&firstno);
+printf("Enter the secondno ");
+scanf("%d",&secondno);
+int result=firstno/secondno;
+printf("Division of the two number is %d", result);
+}
+    // get two values from the user and print it and scan it
+#include <stdio.h>
+int main() {
+int firstno;
+int secondno;
+printf("Enter the firstno ");
+scanf("%d",&firstno);
+printf("Enter the secondno ");
+scanf("%d",&secondno);
+int result=firstno+secondno;
+printf("Addition of the two number is %d", result);
+}
+// get two values from the user and print it and scan it
+#include <stdio.h>
+
+int main() {
+int firstno;
+int secondno;
+printf("Enter the firstno ");
+scanf("%d",&firstno);
+printf("Enter the secondno ");
+scanf("%d",&secondno);
+int result=firstno*secondno;
+printf("Multiple of the two number is %d", result);
+}
+    // get two values from the user and print it and scan it
+#include <stdio.h>
+
+int main() {
+int firstno;
+int secondno;
+printf("Enter the firstno ");
+scanf("%d",&firstno);
+printf("Enter the secondno ");
+scanf("%d",&secondno);
+int result=firstno-secondno;
+printf("Sub of the two number is %d", result);
+}
+    // get three values from the user and print it and scan it
+#include <stdio.h>
+
+int main() {
+int firstno;
+int secondno;
+int thirdno;
+printf("Enter the firstno ");
+scanf("%d",&firstno);
+printf("Enter the secondno ");
+scanf("%d",&secondno);
+printf("Enter the thirdno ");
+scanf("%d",&thirdno);
+int result=firstno+secondno+thirdno;
+printf("Addition of the two number is %d", result);
+}
+    // get three values from the user and print it and scan it
+#include <stdio.h>
+
+int main() {
+int firstno;
+int secondno;
+int thirdno;
+printf("Enter the firstno ");
+scanf("%d",&firstno);
+printf("Enter the secondno ");
+scanf("%d",&secondno);
+printf("Enter the thirdno ");
+scanf("%d",&thirdno);
+int result=firstno-secondno-thirdno;
+printf("sub of the two number is %d", result);
+
+    
+    
