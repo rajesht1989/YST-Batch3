e037ba4fa870df42ac787ad6753b97810791174e
Nagarasu0408 <88180695+Nagarasu0408@users.noreply.github.com>
2021-08-03 22:53:43 +0530

    Create familymembername.c

---
+++ familymembername.c
@@ -0,0 +1,21 @@
+//REQUIREMENT:PRINT OUR FAMILY MEMBER NAME
+
+#include<stdio.h>
+int main(){
+    char a[100];
+    char b[100];
+    char c[100];
+    char d[100];
+    printf("ENTER OUR FAMILY MEMBER NAME\n");
+    printf("what is your name\?\n");
+    scanf("%s",&a);
+    printf("what is your father name\?\n");
+    scanf("%s",&b);
+    printf("what is your mother name\?\n");
+    scanf("%s",&c);
+    printf("what is your brother name\?\n");
+    scanf("%s",&d);
+    char r[1000]="\tHi Nagaraj your family detail prefect";
+    printf("%s",r);
+    
+}
