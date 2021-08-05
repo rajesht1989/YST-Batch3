e3d687be065557a1082078e40fdeaba2511eb22e
Nagarasu0408 <88180695+Nagarasu0408@users.noreply.github.com>
2021-08-05 21:14:28 +0530

    Update day 8:get2&3numbersandaddthem.c

--- day 8:get2&3numbersandaddthem.c
+++ day 8:get2&3numbersandaddthem.c
@@ -35,4 +35,22 @@ int main(){
     printf("VALUE OF ADDITION %d",v);
 }
 
-
+//REQUIREMENT:GET 4 NUMBER FROM USER AND PRINT ADD VALUE
+int getNumberFromUser(){
+    int value;
+    printf("ENTER YOUR NUMBER ");                                           //GET 4 NUMBER FROM USER AND PRINT ADD VALUE
+    scanf("%d",&value);
+    return value;
+}
+int addFourNumber(int first,int second,int third,int four){
+    return first+second+third+four;
+}
+#include<stdio.h>
+int main(){
+    int a=getNumberFromUser();
+    int b=getNumberFromUser();
+    int c=getNumberFromUser();
+    int d=getNumberFromUser();
+    int result=addFourNumber(a,b,c,d);
+    printf("ADD VALUE IS %d",result);
+}
