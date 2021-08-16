8655bb1c09684002ee9655e4ba69b1e98c1dd968
Ajay0978 <88025421+Ajay0978@users.noreply.github.com>
2021-08-16 22:38:18 +0530

    Create Eligible for marriage.C

---
+++ Eligible for marriage.C
@@ -0,0 +1,31 @@
+//Get an age from user and identify if eligible for marriage
+
+#include <stdio.h>
+
+int getAgeFromUser(){
+    int value;
+    printf("Enter the Age");
+    scanf("%d",& value);
+    return value;
+}
+
+
+int eligibleForMarriage(){
+    if ("my Age >= 18"){
+        return 1;
+        }
+        else{
+            return 0;
+        }
+            
+}
+
+void main(){
+    int a=getAgeFromUser();
+      if (eligibleForMarriage()==1){
+      printf("I'm Eligible");
+      }
+      else{
+    printf("I'm not Eligible");
+    }
+}
