6d958de8521603f42640afc1df037b733e879c3c
NAGARASU.P <88180695+Nagarasu0408@users.noreply.github.com>
2021-09-01 23:16:02 +0530

    Create square with slash .c

---
+++ LOOP/HOLLOW SHAPE/square with slash .c
@@ -0,0 +1,27 @@
+//.REQUIREMENT:GET NUMBER FROM USER AND DRAW SQUARE WITH ONESIDE SLASH
+int getNumberFromUser(){
+    int number;
+    printf("Enter a Number ");
+    scanf("%d",&number);
+    return number;
+}
+void draw(int a){
+    for(int i=0;i<a;i++){
+     for(int j=0;j<a;j++){
+    //printf("+\t");
+//printf("\n");
+    if(j==0||i==a-1||i==0||j==a-1||i==j){
+        printf("*\t");
+    }
+    else{
+        printf("\t");
+    }
+    }
+    printf("\n");
+}
+}
+
+#include<stdio.h>
+void main(){
+    draw(getNumberFromUser());
+}
