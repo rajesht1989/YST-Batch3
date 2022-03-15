51d6906a3c34f15d7b1c6196e2f83e6a4178ac93
Vasu3337 <89373680+Vasu3337@users.noreply.github.com>
2022-03-15 10:53:35 +0530

    Create Add2Numbers.c

---
+++ Add2Numbers.c
@@ -0,0 +1,11 @@
+// Requirement: Add 2 numbers 
+#include <stdio.h>
+
+int main() {
+    printf("Addition of 2 Numbers are %d", add(3,4));
+    return 0;
+}
+
+int add(int a, int b) {
+    return a + b;
+}
