e7b07b456948028a66c3bd8bf537a647d33f06b3
Vasu3337 <89373680+Vasu3337@users.noreply.github.com>
2022-04-23 11:25:35 +0530

    accquipo source added

---
+++ Accquipo/Accquipo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Accquipo
+//
+//  Created by apple on 23/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
